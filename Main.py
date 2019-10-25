7a025ca13aa83aa7e7d9eb800f6bde92802009b8
Lostman <36179094+SrLostman@users.noreply.github.com>
2019-10-25 13:14:32 -0500

    1.2

--- Main.py
+++ Main.py
@@ -21,29 +21,30 @@ def sep_column(mat):
 
 def gram_schmidt(A):
     n = A.shape[1]  # numero de vectores columna
-    for j in range(n):
-        for k in range(j):
-            print(f"La division es: {producint(A[::-1, j], A[::-1, k]) / producint(A[::-1, k], A[::-1, k])}")
-            print(f"Y se multiplica por: {A[:, k]}")
-            print(f"El resultado es {(producint(A[::-1, j], A[::-1, k]) / producint(A[::-1, k], A[::-1, k])) * A[:, k]}")
-            print(f"Todo se resta a {A[:,j]}")
-            A[:, j] -= (producint(A[::-1, j], A[::-1, k]) / producint(A[::-1, k], A[::-1, k])) * A[:, k]
-
-            print(f"Este es el vector columa resultante: {A[:,j]}\n")
+    buffer = A[::-1, 0]
+    for j in range(n):  # 0,1,2 numero de vectores
+        for k in range(j):  # j = 0. 0,1. 0,1,2 numero de restas
+                if k < 1:
+                    buffer =  producint(A[::-1, k], A[::-1, j]) / producint(A[::-1, k], A[::-1, k]) * A[::-1, k]
+                else:
+                    buffer1 = (producint(A[::-1, k], A[::-1, j]) / producint(A[::-1, k], A[::-1, k])) * A[::-1, k]
+                    buffer = buffer - buffer1
+        if (j>=1):
+            A[::-1, j] =  A[::-1, j] - buffer
     return A
 
 
 def producint(f1, f2):
-    integral = np.polyint(P.polymul(f1, f2))
+    integral = np.polyint(np.polymul(f1, f2))
     value = (np.polyval(integral, 1) - np.polyval(integral, -1))
     return value
 
 
 if __name__ == '__main__':
+    #B = np.array([[1,2,1],[0,1,-1,],[0,0,1]], np.float)
+    #print(gram_schmidt(B))
     g = int(input("ingrese el grado maximo de los polinomios"))
     r = int(input("ingrese el numero de polinomios"))
     mat = Lectura(r, g)
     print(mat)
     print(gram_schmidt(sep_column(mat)))
-
-
